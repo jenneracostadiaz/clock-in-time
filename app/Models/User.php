d432321d0db7bb90f41cfa78b80d68c556976bf7
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-13 16:50:34 -0500

    feat: install directorytree/authorization package

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Devdojo\Auth\Models\User as AuthUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends AuthUser implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Authorizable, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
