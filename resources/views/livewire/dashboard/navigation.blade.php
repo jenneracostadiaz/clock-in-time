0280c0871e36d6d2510812cae0020ce6ad5c17f0
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-09-13 15:18:27 -0500

    chore: Update dashboard main menu

--- resources/views/livewire/dashboard/navigation.blade.php
+++ resources/views/livewire/dashboard/navigation.blade.php
@@ -1,7 +1,8 @@
 <nav class="bg-cyan-900 text-white w-full p-4 flex justify-between items-center gap-4">
     <h1 class="font-black text-2xl">Dashboard Control</h1>
     <ul class="flex-1 flex gap-4 text-sm font-thin border-l border-white pl-4">
-        <li><a>My Registers</a></li>
+        <li><a href="#">Attendance</a></li>
+        <li><a href="#">Records</a></li>
     </ul>
     <ul class="flex gap-4 text-sm font-thin">
         <li>Hello, <a href="#">{{$user->name}} ({{$user->email}})</a></li>
