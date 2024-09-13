eb3334140d6652449739eff441ee77fc6dceb068
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-09-13 17:23:46 -0500

    feat: Optimizate Button Animation

--- resources/views/livewire/dashboard/check-in.blade.php
+++ resources/views/livewire/dashboard/check-in.blade.php
@@ -1,16 +1,21 @@
 <div class="w-full flex flex-col items-center">
-    <div
-        class="w-[330px] h-[330px] bg-gradient-to-r from-cyan-800 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center transition-all hover:bg-gradient-to-tl">
+    <button class="relative w-[330px] h-[330px] [&>div]:hover:rotate-45 [&>div>div]:hover:rotate-90 [&>div>div>div]:hover:rotate-45 [&>div]:focus:-rotate-45 [&>div>div]:focus:-rotate-90 [&>div>div>div]:focus:-rotate-45
+    " wire:click="checkIn">
         <div
-            class="w-[320px] h-[320px] bg-white rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center">
-            <button
-                class="w-[310px] h-[310px] bg-cyan-900 bg-gradient-to-r from-cyan-600 rounded-full text-white flex flex-col justify-center items-center relative transition-all hover:bg-gradient-to-bl"
-                wire:click="checkIn"
-            >
-                <span class="text-5xl">00:00:00</span>
-            </button>
+            id="bg-bottom"
+            class="w-[330px] h-[330px] bg-gradient-to-r from-cyan-800 to-cyan-50 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center transition-all rotate-0">
+            <div
+                id="bg-center"
+                class="w-[320px] h-[320px] bg-gradient-to-r from-white to-cyan-50 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center rotate-0">
+                <div
+                    id="bg-font"
+                    class="w-[310px] h-[310px] bg-gradient-to-r from-cyan-900 to-cyan-700 rounded-full text-white flex flex-col justify-center items-center transition-all rotate-0 hover:rotate-12"
+                >
+                </div>
+            </div>
         </div>
-    </div>
+        <span class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center text-white text-5xl">00:00:00</span>
+    </button>
 
     <input wire:model="today" name="today" id="today"
            class="border-0 w-full text-center mt-8 bg-transparent capitalize text-slate-600 text-lg focus:ring-0"
