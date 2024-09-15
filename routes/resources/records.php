6543a04ab0486476faacf4c835482d5e319501d2
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-14 19:50:17 -0500

    feat: add route for check-out

--- routes/resources/records.php
+++ routes/resources/records.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Records\StoreCheckInController;
+use App\Http\Controllers\Records\StoreCheckOutController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -9,4 +10,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Register Attendance Entrance
     Route::post('/check-in', StoreCheckInController::class)->name('check-in');
+
+    // Register Attendance Exit
+    Route::patch('/check-out', StoreCheckOutController::class)->name('check-out');
 });
