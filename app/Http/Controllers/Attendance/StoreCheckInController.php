af5ccb8ede6b99ebd9e1662af6ae7be6910d292d
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-14 23:40:29 -0500

    fix: use action

--- app/Http/Controllers/Attendance/StoreCheckInController.php
+++ app/Http/Controllers/Attendance/StoreCheckInController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Attendance;
 
 use App\Http\Controllers\Controller;
+use ClockInTime\Modules\Attendance\Actions\CreateNewAttendanceRecord;
 use ClockInTime\Modules\Attendance\Data\NewCheckInRecord;
-use ClockInTime\Modules\Attendance\Services\AttendanceService;
 use ClockInTime\Modules\Core\Enums\Http;
 use Illuminate\Http\Response;
 
 class StoreCheckInController extends Controller
 {
     public function __construct(
-        public readonly AttendanceService $service
+        public readonly CreateNewAttendanceRecord $createNewAttendanceRecord
     ) {}
 
     /**
@@ -19,7 +19,10 @@ class StoreCheckInController extends Controller
      */
     public function __invoke(NewCheckInRecord $checkInRecord): Response
     {
-        $record = $this->service->checkIn();
+        $record = $this->createNewAttendanceRecord->handle(
+            user: auth()->user(),
+            checkInRecord: $checkInRecord
+        );
 
         return response(['attendance' => $record], Http::CREATED->value);
     }
