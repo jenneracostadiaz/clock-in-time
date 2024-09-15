e7afb437697a8b83e83bb8cc1e024b4143c620fb
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-14 19:44:37 -0500

    feat: add action to update attendance record for check-out

---
+++ modules/Attendance/Actions/UpdateAttendanceRecord.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ClockInTime\Modules\Attendance\Actions;
+
+use ClockInTime\Modules\Attendance\Data\AttendanceRecord;
+use ClockInTime\Modules\Attendance\Data\NewCheckOutRecord;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class UpdateAttendanceRecord
+{
+    public function handle(Authenticatable $user, NewCheckOutRecord $checkOutRecord): AttendanceRecord
+    {
+        $attendance = $user->attendances()->findOrFail($checkOutRecord->id);
+
+        $attendance->update(
+            array_merge($checkOutRecord->toArray(), ['attended' => true])
+        );
+
+        return AttendanceRecord::from(
+            $attendance->fresh()
+        );
+    }
+}
