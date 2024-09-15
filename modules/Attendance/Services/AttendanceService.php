aec6cb342747fa13fa25ba4254ec8593fff345cc
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-14 19:49:16 -0500

    feat: add check-out

--- modules/Attendance/Services/AttendanceService.php
+++ modules/Attendance/Services/AttendanceService.php
@@ -4,8 +4,10 @@ namespace ClockInTime\Modules\Attendance\Services;
 
 use Carbon\Carbon;
 use ClockInTime\Modules\Attendance\Actions\CreateNewCheckInRecord;
+use ClockInTime\Modules\Attendance\Actions\UpdateAttendanceRecord;
 use ClockInTime\Modules\Attendance\Data\AttendanceRecord;
 use ClockInTime\Modules\Attendance\Data\NewCheckInRecord;
+use ClockInTime\Modules\Attendance\Data\NewCheckOutRecord;
 use ClockInTime\Modules\Attendance\Enums\AttendanceStatus;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +15,15 @@ final readonly class AttendanceService
 {
     public function __construct(
         public CreateNewCheckInRecord $createNewCheckInRecord,
+        private UpdateAttendanceRecord $updateAttendanceRecord
     ) {}
+
+    public function findAttendance(Carbon $date): AttendanceRecord
+    {
+        $attendance = Auth::user()->attendances()->where('attendance_date', $date)->firstOrFail();
+
+        return AttendanceRecord::from($attendance);
+    }
 
     public function checkIn(): AttendanceRecord
     {
@@ -26,6 +36,24 @@ final readonly class AttendanceService
         return $this->createNewCheckInRecord->handle(
             user: Auth::user(),
             checkInRecord: $checkInRecord
+        );
+    }
+
+    public function checkOut(): AttendanceRecord
+    {
+        $attendance = $this->findAttendance(
+            Carbon::now()
+        );
+
+        $checkOutRecord = new NewCheckOutRecord(
+            id: $attendance->id,
+            check_out_time: Carbon::now()->format('Y-m-d H:i:s'),
+            status: AttendanceStatus::FINISHED
+        );
+
+        return $this->updateAttendanceRecord->handle(
+            user: Auth::user(),
+            checkOutRecord: $checkOutRecord
         );
     }
 }
