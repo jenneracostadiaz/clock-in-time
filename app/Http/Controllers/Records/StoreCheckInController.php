03afe12c7c1aeec3d974fe371393309cba36df40
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-14 19:01:22 -0500

    feat: use AttendanceService to perform check-in

--- app/Http/Controllers/Records/StoreCheckInController.php
+++ app/Http/Controllers/Records/StoreCheckInController.php
@@ -3,23 +3,23 @@
 namespace App\Http\Controllers\Records;
 
 use App\Http\Controllers\Controller;
-use ClockInTime\Modules\Attendance\Data\AttendanceRecord;
 use ClockInTime\Modules\Attendance\Data\NewCheckInRecord;
+use ClockInTime\Modules\Attendance\Services\AttendanceService;
 use ClockInTime\Modules\Core\Enums\Http;
 use Illuminate\Http\Response;
 
 class StoreCheckInController extends Controller
 {
+    public function __construct(
+        public readonly AttendanceService $service
+    ) {}
+
     /**
      * Handle the incoming request.
      */
     public function __invoke(NewCheckInRecord $checkInRecord): Response
     {
-        $attendance = auth()->user()->attendances()->create(
-            $checkInRecord->toArray()
-        );
-
-        $record = AttendanceRecord::from($attendance);
+        $record = $this->service->checkIn();
 
         return response(['attendance' => $record], Http::CREATED->value);
     }
