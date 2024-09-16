ce5db9e9ad814a320bcfce11e815cb6d08f55af2
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 13:33:29 -0500

    fix: add transformers to ensure datetime formats

--- modules/Attendance/Data/AttendanceRecord.php
+++ modules/Attendance/Data/AttendanceRecord.php
@@ -5,8 +5,10 @@ namespace ClockInTime\Modules\Attendance\Data;
 use ClockInTime\Modules\Attendance\Enums\AttendanceStatus;
 use Illuminate\Support\Carbon;
 use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Attributes\WithTransformer;
 use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Transformers\DateTimeInterfaceTransformer;
 
 class AttendanceRecord extends Data
 {
@@ -14,8 +16,11 @@ class AttendanceRecord extends Data
         public readonly int $id,
         public readonly int $user_id,
         #[WithCast(DateTimeInterfaceCast::class, format: 'Y-m-d')]
+        #[WithTransformer(DateTimeInterfaceTransformer::class, format: 'Y-m-d')]
         public readonly Carbon $attendance_date,
+        #[WithTransformer(DateTimeInterfaceTransformer::class, format: 'Y-m-d H:i:s')]
         public readonly Carbon $check_in_time,
+        #[WithTransformer(DateTimeInterfaceTransformer::class, format: 'Y-m-d H:i:s')]
         public readonly ?Carbon $check_out_time,
         public readonly ?string $remark,
         public readonly ?bool $attended,
