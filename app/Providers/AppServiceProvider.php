e8e8d946115b45bb8ac023ab2cefd83c5a06e821
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 13:41:44 -0500

    feat: add prefetch for vite assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         // Database
         Model::shouldBeStrict();
+
+        // Assets
+        Vite::prefetch(concurrency: 3);
     }
 }
