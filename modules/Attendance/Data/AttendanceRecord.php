13d3cd5b15fc7b38875775d81cd7a37acd3d1bb5
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 12:27:34 -0500

    fix: update types

--- modules/Attendance/Data/AttendanceRecord.php
+++ modules/Attendance/Data/AttendanceRecord.php
@@ -3,6 +3,9 @@
 namespace ClockInTime\Modules\Attendance\Data;
 
 use ClockInTime\Modules\Attendance\Enums\AttendanceStatus;
+use Illuminate\Support\Carbon;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 
 class AttendanceRecord extends Data
@@ -10,9 +13,10 @@ class AttendanceRecord extends Data
     public function __construct(
         public readonly int $id,
         public readonly int $user_id,
-        public readonly string $attendance_date,
-        public readonly string $check_in_time,
-        public readonly ?string $check_out_time,
+        #[WithCast(DateTimeInterfaceCast::class, format: 'Y-m-d')]
+        public readonly Carbon $attendance_date,
+        public readonly Carbon $check_in_time,
+        public readonly ?Carbon $check_out_time,
         public readonly ?string $remark,
         public readonly ?bool $attended,
         public readonly AttendanceStatus $status,
