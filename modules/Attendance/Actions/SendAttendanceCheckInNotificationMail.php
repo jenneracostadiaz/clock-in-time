eb6b4bbead3fe344778f3a72f0d5e4e253d4dfcd
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 16:42:00 -0500

    chore: remove log

--- modules/Attendance/Actions/SendAttendanceCheckInNotificationMail.php
+++ modules/Attendance/Actions/SendAttendanceCheckInNotificationMail.php
@@ -5,16 +5,11 @@ namespace ClockInTime\Modules\Attendance\Actions;
 use App\Models\User;
 use ClockInTime\Modules\Attendance\Data\AttendanceRecord;
 use ClockInTime\Modules\Attendance\Notifications\AttendanceCheckInConfirmed;
-use Illuminate\Support\Facades\Log;
 
 class SendAttendanceCheckInNotificationMail
 {
     public function handle(AttendanceRecord $attendance): void
     {
-        $user = User::query()->findOrFail($attendance->user_id);
-
-        Log::debug(__METHOD__, [$user]);
-
         User::query()->findOrFail($attendance->user_id)->notify(
             new AttendanceCheckInConfirmed($attendance)
         );
