84cc7a3178cb2670f2310a4512eef6ca8f4b1135
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 17:12:10 -0500

    refactor: extract background to component

--- resources/views/livewire/dashboard/user-attendances.blade.php
+++ resources/views/livewire/dashboard/user-attendances.blade.php
@@ -5,19 +5,8 @@
         @if($showButton === 'checkOut') wire:click="checkOut" @endif
         @if($showButton === 'resume') wire:click="showResume" @endif
     >
-        <div
-            id="bg-bottom"
-            class="w-[330px] h-[330px] bg-gradient-to-r from-cyan-800 to-cyan-50 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center transition-all rotate-0">
-            <div
-                id="bg-center"
-                class="w-[320px] h-[320px] bg-gradient-to-r from-white to-cyan-50 rounded-full text-white text-2xl font-bold  flex justify-center items-center rotate-0">
-                <div
-                    id="bg-font"
-                    class="w-[310px] h-[310px] bg-gradient-to-r from-cyan-900 to-cyan-700 rounded-full text-white flex flex-col justify-center items-center transition-all rotate-0 hover:rotate-12"
-                >
-                </div>
-            </div>
-        </div>
+        <x-timer-display-background />
+
         <section class="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center text-white">
             <p class="text-xs pt-4"> {{$up_title}}</p>
             <p>
