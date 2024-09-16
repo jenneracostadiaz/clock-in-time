ccdc5be8a8b47965d63c631cdd4ff57606455d97
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 17:22:16 -0500

    refactor: simplify classes

--- resources/views/components/timer-display-background.blade.php
+++ resources/views/components/timer-display-background.blade.php
@@ -1,12 +1,12 @@
 <div
     id="bg-bottom"
-    class="w-[330px] h-[330px] bg-gradient-to-r from-cyan-800 to-cyan-50 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center transition-all rotate-0">
+    class="size-[330px] bg-gradient-to-r from-cyan-800 to-cyan-50 rounded-full text-white text-2xl font-bold shadow-2xl flex justify-center items-center transition-all rotate-0">
     <div
         id="bg-center"
-        class="w-[320px] h-[320px] bg-gradient-to-r from-white to-cyan-50 rounded-full text-white text-2xl font-bold  flex justify-center items-center rotate-0">
+        class="size-[320px] bg-gradient-to-r from-white to-cyan-50 rounded-full text-white text-2xl font-bold  flex justify-center items-center rotate-0">
         <div
             id="bg-font"
-            class="w-[310px] h-[310px] bg-gradient-to-r from-cyan-900 to-cyan-700 rounded-full text-white flex flex-col justify-center items-center transition-all rotate-0 hover:rotate-12"
+            class="size-[310px] bg-gradient-to-r from-cyan-900 to-cyan-700 rounded-full text-white flex flex-col justify-center items-center transition-all rotate-0 hover:rotate-12"
         >
         </div>
     </div>
