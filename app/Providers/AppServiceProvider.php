4b18f0c503953a03dc274925aaecf742cf15ec85
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 13:43:52 -0500

    feat: register timezone macros

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,23 @@
 
 namespace App\Providers;
 
+use App\Support\Traits\WithTimezoneDisplay;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    use WithTimezoneDisplay;
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        // Macros
+        $this->registerInApplicationTimezone();
+        $this->registerInUserTimezone();
     }
 
     /**
