086434bc1e0d90f92a090668c9814f974560fd9b
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 13:36:05 -0500

    fix: remove id

--- tests/Unit/Attendance/NewCheckOutRecordTest.php
+++ tests/Unit/Attendance/NewCheckOutRecordTest.php
@@ -5,7 +5,6 @@ use ClockInTime\Modules\Attendance\Enums\AttendanceStatus;
 
 it('can create a new check out record', function () {
     $record = new NewCheckOutRecord(
-        id: 1,
         check_out_time: '2024-09-17 08:00:00',
         status: AttendanceStatus::FINISHED,
     );
