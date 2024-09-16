717338fafb9538d6758b4265f853300b6c2cde24
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 16:56:06 -0500

    feat: add event on check-out registered

---
+++ app/Events/AttendanceCheckOutRegistered.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use ClockInTime\Modules\Attendance\Data\AttendanceRecord;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AttendanceCheckOutRegistered implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly AttendanceRecord $attendance) {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('attendances'),
+        ];
+    }
+}
