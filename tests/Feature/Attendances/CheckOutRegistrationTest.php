3dc3f5a952490b26159e4fd0146d5534a43f7551
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 21:44:02 -0500

    refactor: extract tests in different files

--- tests/Feature/Attendances/CheckOutRegistrationTest.php
+++ tests/Feature/Attendances/CheckOutRegistrationTest.php
@@ -5,34 +5,6 @@ use ClockInTime\Modules\Core\Enums\Http;
 
 use function Pest\Laravel\actingAs;
 
-test('an authenticated user can check in his attendance entrance', function ($checkInRecord) {
-    $user = User::factory()->create();
-
-    $response = actingAs($user)->postJson('/records/check-in', [
-        'attendance_date' => $checkInRecord->attendance_date,
-        'check_in_time' => $checkInRecord->check_in_time,
-        'status' => $checkInRecord->status,
-    ]);
-
-    expect($response->status())->toBe(Http::CREATED->value)
-        ->and($response)->assertJson([
-            'attendance' => [
-                'user_id' => $user->id,
-                'attendance_date' => $checkInRecord->attendance_date,
-                'check_in_time' => $checkInRecord->check_in_time,
-                'check_out_time' => null,
-                'attended' => false,
-                'status' => $checkInRecord->status,
-            ],
-        ]);
-})->with([
-    'checkInRecord' => (object) [
-        'attendance_date' => '2024-09-17',
-        'check_in_time' => '2024-09-17 08:00:00',
-        'status' => 'started',
-    ],
-]);
-
 test('an authenticated user can check out his attendance exit', function ($checkOutRecord) {
     $this->withoutExceptionHandling();
     $user = User::factory()->create();
