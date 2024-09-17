5e103fc9847627511a9f4b8412fa217046eb7299
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2024-09-17 09:48:11 -0500

    feat: Update size fonts in navigation

--- resources/views/livewire/dashboard/navigation.blade.php
+++ resources/views/livewire/dashboard/navigation.blade.php
@@ -1,10 +1,12 @@
-<nav class="bg-cyan-900 text-white w-full p-4 flex justify-between items-center gap-4">
-    <h1 class="font-black text-2xl">{{ config('app.name', 'Laravel') }}</h1>
-    <ul class="flex-1 flex gap-4 text-sm font-thin border-l border-white pl-4">
+<nav class="bg-cyan-900 text-white w-full p-6 flex justify-between items-center gap-6">
+    <h1 class="font-black text-2xl">
+        <a href="{{ route('dashboard') }}">{{ config('app.name', 'Laravel') }}</a>
+    </h1>
+    <ul class="flex-1 flex gap-4 font-thin border-l border-white pl-4">
         <li><a href="{{ route('dashboard') }}">Attendance</a></li>
         <li><a href="{{ route('records:index') }}">Records</a></li>
     </ul>
-    <ul class="flex gap-4 text-sm font-thin">
+    <ul class="flex gap-4 font-thin">
         <li>Hello, <a href="#">{{$user->name}} ({{$user->email}})</a></li>
         <li><a href="{{ route('logout')  }}">Logout</a></li>
     </ul>
