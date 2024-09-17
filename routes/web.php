cda9678e88dee4270528c9b7214861ca5b723d57
Monassi Carrasco <mon4ssi@outlook.com>
2024-09-16 22:16:57 -0500

    refactor: organize routes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::any('/register', fn () => redirect('login'))->name('register.disabled');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
-    Route::get('/records', function () {
-        return view('records');
-    });
-    Route::get('/reports', function () {
-        return view('reports');
-    });
+    Route::get('/', fn () => view('dashboard'))->name('dashboard');
 });
+
+Route::prefix('reports')->as('reports:')->group(
+    base_path('routes/resources/reports.php')
+);
 
 Route::prefix('records')->as('records:')->group(
     base_path('routes/resources/records.php')
